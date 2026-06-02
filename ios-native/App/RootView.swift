3f102faf47737702445ebe492b6abafc2fdd886c
mcguinn620-netizen <mcguinn620@hotmail.com>
2026-06-01 23:47:01 -0400

    Update RootView.swift

--- ios-native/App/RootView.swift
+++ ios-native/App/RootView.swift
@@ -3,16 +3,56 @@ import SwiftUI
 struct RootView: View {
     @EnvironmentObject private var appState: AppState
 
+    #if DEBUG
+    @AppStorage("debugLoginBypass")
+    private var debugLoginBypass = true
+    #endif
+
     var body: some View {
+
+        #if DEBUG
+
+        if debugLoginBypass {
+
+            MainTabView()
+
+        } else {
+
+            Group {
+                switch appState.authState {
+
+                case .loading:
+                    ProgressView("Loading")
+
+                case .signedOut:
+                    AuthView(
+                        viewModel: AuthViewModel()
+                    )
+
+                case .signedIn:
+                    MainTabView()
+                }
+            }
+        }
+
+        #else
+
         Group {
             switch appState.authState {
+
             case .loading:
                 ProgressView("Loading")
+
             case .signedOut:
-                AuthView(viewModel: AuthViewModel())
+                AuthView(
+                    viewModel: AuthViewModel()
+                )
+
             case .signedIn:
                 MainTabView()
             }
         }
+
+        #endif
     }
 }
